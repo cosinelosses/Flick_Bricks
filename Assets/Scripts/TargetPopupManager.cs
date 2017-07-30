dd4d1a22a545edf581bf071ed1c188dc6ab7b7d9
matthew <dev@cosinelosses.com>
2017-07-29 20:27:02 -0400

    -update
    -respawn after 3 secs

--- Assets/Scripts/TargetPopupManager.cs
+++ Assets/Scripts/TargetPopupManager.cs
@@ -10,6 +10,9 @@ public class TargetPopupManager : MonoBehaviour {
 
     // platform for reference
     public GameObject ref_platform;
+    
+
+    // set and make private
     public float distance_ahead;
     public float distance_up;
 
@@ -21,13 +24,17 @@ public class TargetPopupManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        // initialize time
         time = 0;
-        targ_ring = (GameObject)Instantiate(target_prefab, new Vector3() + new Vector3(0, distance_up, distance_ahead),
-            transform.rotation);
-	}
+
+        // spawn initial ring
+        targ_ring = (GameObject)Instantiate(target_prefab, new Vector3() + new Vector3(0,
+                distance_up, distance_ahead), transform.rotation);
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        // move the ring at same (will be less) rate as platform
         targ_ring.transform.Translate(Vector3.forward * 10 * Time.deltaTime, Space.World);
     }
 
@@ -35,15 +42,26 @@ public class TargetPopupManager : MonoBehaviour {
     {
         time += Time.deltaTime;
 
-        Math.Round(time, 0); 
-    }
+        if(Math.Round(time, 0) > visible_duration)
+        {
+            // destroy existing 
+            Destroy(targ_ring);
 
-    private int GenRandNum(int min, int max)
+            // create new
+            targ_ring = (GameObject)Instantiate(target_prefab, new Vector3(ref_platform.transform.position.x,
+                ref_platform.transform.position.y, ref_platform.transform.position.z) +
+                new Vector3(0, distance_up, distance_ahead), transform.rotation);
+
+            time = 0; 
+        }
+    }   
+
+    private int genRandNum(int min, int max)
     {        
         System.Random randNumGen = new System.Random();
         int rand = randNumGen.Next(min, max);
 
         print("Random number is: " + rand);
         return rand; 
-    }
+    }  
 }
