48813c07c7338e8cb7fee5e9f510f985c0c35153
matthew <dev@cosinelosses.com>
2017-07-29 23:21:22 -0400

    rands added
    single ring spawns in random locations within 3 specified ranges
    -for each axis

--- Assets/Scripts/TargetPopupManager.cs
+++ Assets/Scripts/TargetPopupManager.cs
@@ -11,13 +11,14 @@ public class TargetPopupManager : MonoBehaviour {
     // platform for reference
     public GameObject ref_platform;
     
-
-    // set and make private
-    public float distance_ahead;
-    public float distance_up;
+    // set and make private -set by randoms     
+    private float distance_z;
+    private float distance_x;
+    private float distance_y;
 
     public float visible_duration;
     private double time;
+    private double time2; 
 
     // current target
     private GameObject targ_ring;    
@@ -27,9 +28,12 @@ public class TargetPopupManager : MonoBehaviour {
         // initialize time
         time = 0;
 
+        // gen 3 rands for x, y, z
+        setRandInts(); 
+
         // spawn initial ring
         targ_ring = (GameObject)Instantiate(target_prefab, new Vector3() + new Vector3(0,
-                distance_up, distance_ahead), transform.rotation);
+                distance_y, distance_z), transform.rotation);
     }
 	
 	// Update is called once per frame
@@ -47,13 +51,25 @@ public class TargetPopupManager : MonoBehaviour {
             // destroy existing 
             Destroy(targ_ring);
 
+            distance_z = genRandNum(20, 150);
+            distance_y = genRandNum(0, 20);
+            distance_x = genRandNum(-15, 15);
+            
+            print("distance_z is now: " + distance_z + " distance_x is now: " + distance_x);
+
             // create new
             targ_ring = (GameObject)Instantiate(target_prefab, new Vector3(ref_platform.transform.position.x,
                 ref_platform.transform.position.y, ref_platform.transform.position.z) +
-                new Vector3(0, distance_up, distance_ahead), transform.rotation);
+                new Vector3(distance_x, distance_y, distance_z), transform.rotation); // replace here (up/ahead) 
 
             time = 0; 
         }
+
+        /*
+        if(Math.Round(time2, 0) % 2 == 0)
+        {
+            genRandNum(20, 40); 
+        }*/
     }   
 
     private int genRandNum(int min, int max)
@@ -64,4 +80,11 @@ public class TargetPopupManager : MonoBehaviour {
         print("Random number is: " + rand);
         return rand; 
     }  
+
+    private void setRandInts()
+    {
+        distance_z = genRandNum(20, 150);
+        distance_y = genRandNum(0, 20);
+        distance_x = genRandNum(-15, 15);
+    }
 }
